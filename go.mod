77fcd385356e5a863ba8a76247ca693e2530de59
BachErik <bacherik.info@gmail.com>
2026-07-18 21:20:12 +0200

    update ebiten and add build script

--- go.mod
+++ go.mod
@@ -2,12 +2,14 @@ module bluescreen
 
 go 1.25.0
 
-require github.com/hajimehoshi/ebiten v1.12.12
+require github.com/hajimehoshi/ebiten/v2 v2.9.9
 
 require (
-	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20221017161538-93cebf72946b // indirect
-	golang.org/x/exp v0.0.0-20200224162631-6cc2880d07d6 // indirect
+	github.com/ebitengine/gomobile v0.0.0-20250923094054-ea854a63cce1 // indirect
+	github.com/ebitengine/hideconsole v1.0.0 // indirect
+	github.com/ebitengine/purego v0.9.0 // indirect
+	github.com/jezek/xgb v1.1.1 // indirect
 	golang.org/x/image v0.41.0 // indirect
-	golang.org/x/mobile v0.0.0-20230922142353-e2f452493d57 // indirect
-	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/sync v0.17.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,98 +1,86 @@
 package main
 
 import (
+	"bytes"
+	"embed"
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
+	"io/fs"
 	"log"
-	"math/rand"
-	"os"
-	"path/filepath"
-	"time"
+	"math/rand/v2"
 
-	"github.com/hajimehoshi/ebiten"
-	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2"
 )
 
-var (
-	imagePaths []string
-)
-
-func main() {
-	// Initialisiere den Zufallsgenerator
-	rand.Seed(time.Now().UnixNano())
-
-	// Sammle alle Bildpfade aus dem Unterordner "bluescreens"
-	if err := filepath.Walk("bluescreens", func(path string, info os.FileInfo, err error) error {
-		if !info.IsDir() {
-			imagePaths = append(imagePaths, path)
-		}
-		return nil
-	}); err != nil {
-		log.Fatal(err)
-	}
-
-	// Wähle ein zufälliges Bild aus dem Array
-	randomIndex := rand.Intn(len(imagePaths))
-	selectedImagePath := imagePaths[randomIndex]
-
-	// Lade das ausgewählte Bild
-	img, _, err := ebitenutil.NewImageFromFile(selectedImagePath, ebiten.FilterDefault)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Ermittle die Bildschirmauflösung
-	screenWidth, screenHeight := ebiten.ScreenSizeInFullscreen()
-
-	// Passe die Größe des Bildes an die Bildschirmauflösung an
-	imgWidth, imgHeight := img.Size()
-	scaleX := float64(screenWidth) / float64(imgWidth)
-	scaleY := float64(screenHeight) / float64(imgHeight)
-	scale := scaleX
-	if scaleY < scaleX {
-		scale = scaleY
-	}
-	adjustedWidth := int(float64(imgWidth) * scale)
-	adjustedHeight := int(float64(imgHeight) * scale)
-
-	// Skaliere das Bild auf die angepasste Größe
-	scaledImg, _ := ebiten.NewImage(adjustedWidth, adjustedHeight, ebiten.FilterDefault)
-	op := &ebiten.DrawImageOptions{}
-	op.GeoM.Scale(scale, scale)
-	scaledImg.DrawImage(img, op)
-
-	// Erstelle das Fenster im Vollbildmodus mit angepasster Größe
-	ebiten.SetFullscreen(true)
-	ebiten.SetWindowTitle("Definitiv kein Bluescreen")
-	ebiten.SetWindowSize(screenWidth, screenHeight)
-
-	// Definiere das Spiel
-	game := &Game{
-		img: scaledImg,
-	}
-
-	// Starte das Spiel
-	if err := ebiten.RunGame(game); err != nil {
-		log.Fatal(err)
-	}
-}
+//go:embed bluescreens
+var bluescreensFS embed.FS
 
 type Game struct {
 	img *ebiten.Image
 }
 
-func (g *Game) Update(screen *ebiten.Image) error {
-	// Beende das Spiel, wenn "q" oder "Q" gedrückt wird oder STRG + C betätigt wird
-	if ebiten.IsKeyPressed(ebiten.KeyQ) || ebiten.IsKeyPressed(ebiten.KeyEscape) || (ebiten.IsKeyPressed(ebiten.KeyC) && ebiten.IsKeyPressed(ebiten.KeyControl)) {
-		os.Exit(0)
+func main() {
+	entries, err := fs.ReadDir(bluescreensFS, "bluescreens")
+	if err != nil {
+		log.Fatal(err)
 	}
 
+	var paths []string
+	for _, e := range entries {
+		if !e.IsDir() {
+			paths = append(paths, "bluescreens/"+e.Name())
+		}
+	}
+	if len(paths) == 0 {
+		log.Fatal("keine Bilder in bluescreens/ eingebettet")
+	}
+
+	data, err := bluescreensFS.ReadFile(paths[rand.IntN(len(paths))])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	src, _, err := image.Decode(bytes.NewReader(data))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ebiten.SetFullscreen(true)
+	ebiten.SetWindowTitle("Definitiv kein Bluescreen")
+	ebiten.SetCursorMode(ebiten.CursorModeHidden)
+
+	if err := ebiten.RunGame(&Game{img: ebiten.NewImageFromImage(src)}); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (g *Game) Update() error {
+	if ebiten.IsKeyPressed(ebiten.KeyQ) ||
+		ebiten.IsKeyPressed(ebiten.KeyEscape) ||
+		((ebiten.IsKeyPressed(ebiten.KeyControlLeft) || ebiten.IsKeyPressed(ebiten.KeyControlRight)) &&
+			ebiten.IsKeyPressed(ebiten.KeyC)) {
+		return ebiten.Termination
+	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	// Zeichne das Bild auf den Bildschirm
-	screen.DrawImage(g.img, nil)
+	sw, sh := float64(screen.Bounds().Dx()), float64(screen.Bounds().Dy())
+	iw, ih := float64(g.img.Bounds().Dx()), float64(g.img.Bounds().Dy())
+
+	// "contain": größter Faktor, bei dem das Bild noch komplett passt
+	scale := sw / iw
+	if s := sh / ih; s < scale {
+		scale = s
+	}
+
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Scale(scale, scale)
+	op.GeoM.Translate((sw-iw*scale)/2, (sh-ih*scale)/2) // zentrieren
+	screen.DrawImage(g.img, op)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
-	return ebiten.ScreenSizeInFullscreen()
+	return outsideWidth, outsideHeight
 }
